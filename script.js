33cf64e2d98f6d1f063a04f4e923d705fbe576f8
sajjadmolliek <sajjadmolliek2018@gmail.com>
2023-08-20 21:19:07 +0600

    fixed calculation Issue

--- script.js
+++ script.js
@@ -1,64 +1,71 @@
 
 function clicKoro(idName, innerTxt, price){
     document.getElementById(idName).addEventListener('click', function(){
-        const items_Entry = document.getElementById('items_Entry');
-        const p = document.createElement('p');
+        let items_Entry = document.getElementById('items_Entry');
+        let p = document.createElement('p');
         let count = items_Entry.childElementCount;
         p.innerText = `${count+1}. ${innerTxt}`;
-        const txt = parseFloat(document.getElementById(price).innerText);
-        const total = parseFloat(document.getElementById('total').innerText);
-        const totalPrice = parseFloat(document.getElementById('totalPrice').innerText);
-        const discount = parseFloat(document.getElementById('discount').innerText);
-        const totalSum =  totalPrice+txt;
+        let txt = parseFloat(document.getElementById(price).innerText);
+        let total = parseFloat(document.getElementById('total').innerText);
+        let totalPrice = parseFloat(document.getElementById('totalPrice').innerText);
+        let discount = parseFloat(document.getElementById('discount').innerText);
+        let totalSum =  totalPrice+txt;
         document.getElementById('totalPrice').innerText =totalSum.toFixed(2);
-        const subTotalSum =  total+txt;
+        let subTotalSum =  total+txt;
         (document.getElementById('total').innerText) =subTotalSum.toFixed(2);
         items_Entry.appendChild(p);
 
-        if(totalSum>=200){
-            const applyBtn = document.getElementById("applyBtn");
+
+        if(totalSum >= 200){
+            let applyBtn = document.getElementById("applyBtn");
             applyBtn.style.opacity = 1;
             applyBtn.disabled = false;
-
-            document.getElementById("applyBtn").addEventListener('click', function(){
-                const inputContent = document.getElementById('inputField');
-
-                if(inputContent.value=="SELL200"){
-                    const discountSum = (totalSum*0.2)
-                    document.getElementById('discount').innerText = discountSum.toFixed(2);
-                    document.getElementById('total').innerText =(totalSum - discountSum).toFixed(2);
-                    inputContent.value = "";
+            applyBtn.addEventListener('click', function(){
+                let inputContent = document.getElementById('inputField');
+                
+                if((inputContent.value) === "SELL200"){
+                    
+                    let discount = parseFloat(document.getElementById('discount').innerText);
+                    discount = (totalSum*0.20);
+                    document.getElementById('discount').innerText = discount.toFixed(2);
+                    subTotalSum =  totalSum - discount;
+                    (document.getElementById('total').innerText) =subTotalSum.toFixed(2);
                     
                 }
-            })
-
+            });
             
         }
+
 
         document.getElementById("modalBttn").addEventListener('click', function(){
             items_Entry.removeChild(p);
             p.innerText = "";
-            const afterClickTotal = 0.00;
-            const afterClickTotalPrice = 0.00;
-            const afterClickDiscount = 0.00;
-            document.getElementById('totalPrice').innerText = afterClickTotalPrice.toFixed(2);
-            document.getElementById('discount').innerText = afterClickDiscount.toFixed(2);
-            document.getElementById('total').innerText = afterClickTotal.toFixed(2);
+            (document.getElementById('inputField') ).value= "";
 
-            const MakePurchaseBttn = document.getElementById("MakePurchaseBttn");
+            document.getElementById('totalPrice').innerText = "00.00";
+            document.getElementById('discount').innerText = "00.00";
+            document.getElementById('total').innerText = "00.00";
+
+
+            let MakePurchaseBttn = document.getElementById("MakePurchaseBttn");
             MakePurchaseBttn.style.opacity = 0.3;
             MakePurchaseBttn.disabled = true;
             
-            const applyBtn = document.getElementById("applyBtn");
+            let applyBtn = document.getElementById("applyBtn");
             applyBtn.style.opacity = 0.3;
             applyBtn.disabled = true;
-            })
 
-        const MakePurchaseBttn = document.getElementById("MakePurchaseBttn");
+            
+        });
+
+
+        let MakePurchaseBttn = document.getElementById("MakePurchaseBttn");
         MakePurchaseBttn.style.opacity = 1;
         MakePurchaseBttn.disabled = false;
     });
+
 };
+
 
 
 clicKoro( idName = "pro1", innerTxt = "K. Accessories(1)", price = "price1");
@@ -69,14 +76,11 @@ clicKoro( idName = "pro5", innerTxt = "Full Jersey Set", price = "price5");
 clicKoro( idName = "pro6", innerTxt = "Sports cates", price = "price6");
 
 
-
-
-
-        const MakePurchaseBttn = document.getElementById("MakePurchaseBttn");
+        let MakePurchaseBttn = document.getElementById("MakePurchaseBttn");
         MakePurchaseBttn.style.opacity = 0.3;
         MakePurchaseBttn.disabled = true;
         
-        const applyBtn = document.getElementById("applyBtn");
+        let applyBtn = document.getElementById("applyBtn");
         applyBtn.style.opacity = 0.3;
         applyBtn.disabled = true;
 
